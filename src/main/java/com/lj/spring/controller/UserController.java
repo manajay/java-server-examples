b08c6d7940344672128d0799582662eaf616b288
manajay <manajay.dlj@gmail.com>
2017-08-08 10:43:27 +0800

    fix druid classnotFound issue and solution is change version 1.0.24 download source.jar reimport maven dependencies

--- src/main/java/com/lj/spring/controller/UserController.java
+++ src/main/java/com/lj/spring/controller/UserController.java
@@ -6,8 +6,6 @@ import com.lj.spring.service.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
